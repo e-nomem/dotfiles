62313e7bebf7fdd79a6ac34062c6bbdf38f6ac19
Eashwar Ranganathan <eashwar@eashwar.com>
2018-08-30 13:50:15 -0700

    Initial Commit

---
+++ setup.sh
@@ -0,0 +1,103 @@
+#!/usr/bin/env bash
+
+source_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >>/dev/null && pwd )"
+# Load up the ntasklib and initialize it here
+# Sourcing the file implicitly initializes it here
+. "$source_dir/ntasklib.sh"
+
+# This variable is no longer required once the ntasklib is loaded
+unset "source_dir"
+
+# Ensure that we trap the EXIT signal to clean up all the ntasklib stuff
+trap ntlib_cleanup EXIT
+
+# Utility function to find an executable in the path
+bin_exists() {
+  hash "$1" 2>/dev/null
+}
+
+## ----- Task Definitions Start Here ----- ##
+
+install_homebrew() {
+  if ! bin_exists brew; then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null
+  fi
+}
+task install_homebrew
+
+update_homebrew() {
+  brew update
+}
+task update_homebrew install_homebrew
+
+install_gnupg() {
+  if ! bin_exists gpg; then
+    brew install gnupg
+  fi
+}
+task install_gnupg update_homebrew
+
+install_stow() {
+  if ! bin_exists stow; then
+    brew install stow
+  fi
+}
+task install_stow update_homebrew
+
+install_git() {
+  if ! bin_exists git; then
+    brew install git
+  fi
+}
+task install_git update_homebrew
+
+install_mercurial() {
+  if ! bin_exists hg; then
+    brew install mercurial
+  fi
+}
+task install_mercurial update_homebrew
+
+install_awscli() {
+  if ! bin_exists aws; then
+    brew install awscli
+  fi
+}
+task install_awscli update_homebrew
+
+install_python() {
+  if ! bin_exists python2.7; then
+    brew install python@2
+  fi
+}
+task install_python update_homebrew
+
+create_source_dir() {
+  mkdir -p "$HOME/src"
+}
+task create_source_dir
+
+configure_hgrc() {
+  if [[ -f "$HOME/.hgrc" ]]; then
+    echo "Doing .hgrc stuff"
+  else
+    echo "Not doing .hgrc stuff"
+  fi
+}
+task configure_hgrc install_mercurial
+
+clone_vanguard() {
+  :
+}
+task clone_vanguard configure_hgrc create_source_dir
+
+install_bins() {
+  :
+}
+task install_bins install_git install_mercurial install_awscli install_python
+
+## ----- Task Definitions End Here ----- ##
+
+
+# Go do the things
+run_task install_bins
