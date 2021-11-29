ef3a7f147867fe1580e00057980f96edf5e32be1
Eashwar Ranganathan <eashwar@eashwar.com>
2021-11-29 01:29:26 -0800

    Move more packages to nix

--- nix/.config/nixpkgs/modules/packages.nix
+++ nix/.config/nixpkgs/modules/packages.nix
@@ -3,12 +3,18 @@
   home.packages = with pkgs; [
     bazelisk
     clusterctl
+    deno
     direnv
     jq
     kind
     kubectl
     kubernetes-helm
     mosh
+    pipenv
+    pre-commit
+    pstree
     ripgrep
+    socat
+    step-cli
   ];
 }
