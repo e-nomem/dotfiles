ccf8ff4e78bc26b0937f6b0981ad4f1a8c181fdd
Eashwar Ranganathan <eashwar@eashwar.com>
2024-12-15 00:35:36 -0800

    Updates

--- nix/.config/home-manager/modules/packages.nix
+++ nix/.config/home-manager/modules/packages.nix
@@ -2,7 +2,6 @@
 {
   home.packages = with pkgs; [
     _1password-cli
-    age-plugin-yubikey
     apko
     beancount
     fish
@@ -13,16 +12,12 @@
     melange
     mosh
     nushell
-    pipenv
     pre-commit
     pstree
-    rage
     ripgrep
     sccache
     socat
-#    step-cli
     yt-dlp
-    yubikey-agent
     yubikey-manager
     zig
   ];
